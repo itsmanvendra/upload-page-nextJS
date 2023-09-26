62854b355095c52701848dbdf52e92a4a9ebf624
itsmanvendra <singh03manvendra@gmail.com>
2023-09-26 17:28:16 +0530

    formatting done

--- components/Form.js
+++ components/Form.js
@@ -4,189 +4,189 @@ import { FaFileArrowUp } from "react-icons/fa6";
 import Modal from "./Modal";
 
 export const Form = () => {
-  const fileInputRef = useRef(null);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [fileContent, setFileContent] = useState("");
-  const [nameError, setNameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [parsing, setParsing] = useState(false);
-  const [buttonDisabled, setButtonDisabled] = useState(true);
-  const [isOpen, setIsOpen] = useState(false);
-  const [entriesCount, setEntriesCount] = useState(0);
-  const isEmailValid = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
+    const fileInputRef = useRef(null);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [fileContent, setFileContent] = useState("");
+    const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [parsing, setParsing] = useState(false);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const [entriesCount, setEntriesCount] = useState(0);
+    const isEmailValid = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    if (!isEmailValid(e.target.value)) {
-      setEmailError(true);
-    } else {
-      setEmailError(false);
-    }
-  };
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    console.log(file);
-    setParsing(true);
-      const reader = new FileReader();
-      if (file.type !== "application/json") {
-          alert("Please upload a valid JSON file");
-          setParsing(false);
-          return;
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        if (!isEmailValid(e.target.value)) {
+        setEmailError(true);
+        } else {
+        setEmailError(false);
         }
-    reader.onload = (e) => {
-      setEntriesCount(JSON.parse(e.target.result).length);
-      setFileContent(JSON.stringify(JSON.parse(e.target.result), null, 2));
-      setParsing(false);
     };
-    reader.readAsText(file);
-  };
 
-    const onClose = () => {
-    setIsOpen(false);
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        console.log(file);
+        setParsing(true);
+        const reader = new FileReader();
+        if (file.type !== "application/json") {
+            alert("Please upload a valid JSON file");
+            setParsing(false);
+            return;
+            }
+        reader.onload = (e) => {
+        setEntriesCount(JSON.parse(e.target.result).length);
+        setFileContent(JSON.stringify(JSON.parse(e.target.result), null, 2));
+        setParsing(false);
+        };
+        reader.readAsText(file);
     };
-    
-    const onGoToEntries = () => {
+
+        const onClose = () => {
         setIsOpen(false);
-        window.location.reload();
-    }
+        };
+        
+        const onGoToEntries = () => {
+            setIsOpen(false);
+            window.location.reload();
+        }
 
 
-  useEffect(() => {
-    if (!nameError && name && !emailError && email && fileContent) {
-      console.log("Form is valid");
-      setButtonDisabled(false);
-    } else {
-      console.log("Form is invalid");
-    }
-  }, [nameError, emailError, fileContent, name, email]);
+    useEffect(() => {
+        if (!nameError && name && !emailError && email && fileContent) {
+        console.log("Form is valid");
+        setButtonDisabled(false);
+        } else {
+        console.log("Form is invalid");
+        }
+    }, [nameError, emailError, fileContent, name, email]);
 
-  return (
-    <>
-      <form className="w-full">
-        <div className="flex flex-col mb-2">
-          <label htmlFor="name" className="text-base">
-            Full Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="rounded-md my-2 p-2 bg-slate-50"
-            placeholder="Full Name"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-              setNameError(false);
-            }}
-            onBlur={() => {
-              if (!name) {
-                setNameError(true);
-              }
-            }}
-          />
-          {nameError && (
-            <p className="text-red-500 text-sm">Please enter a name</p>
-          )}
-        </div>
-        <div className="flex flex-col relative mb-2">
-          <label htmlFor="email" className="text-base">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            className="rounded-md my-2 p-2 bg-slate-50"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => handleEmailChange(e)}
-            onBlur={() => {
-              if (!isEmailValid(email)) {
-                setEmailError(true);
-              }
-            }}
-          />
-          <BiEnvelope
-            size={28}
-            className="absolute right-4 top-10 text-sm text-slate-300"
-          />
-          {emailError && (
-            <p className="text-red-500 text-sm">Please enter a valid email</p>
-          )}
-        </div>
-        <div className="flex flex-col relative mb-2">
-          <label htmlFor="uploadFile" className="text-base mb-4">
-            Upload JSON File
-          </label>
-          <div
-            className="w-full h-32 py-8 bg-slate-100 border-2 border-dashed rounded-md"
-            onClick={(e) => {
-              fileInputRef.current.click();
-            }}
-          >
-            {parsing ? (
-              <BiLoaderCircle size={28} className="mx-auto text-blue-500" />
-            ) : (
-              <FaFileArrowUp size={28} className="mx-auto text-blue-500" />
-            )}
-            <div className="text-center">
-              <input
-                type="file"
-                className="hidden"
-                accept="application/JSON"
-                ref={fileInputRef}
+    return (
+        <>
+        <form className="w-full">
+            <div className="flex flex-col mb-2">
+            <label htmlFor="name" className="text-base">
+                Full Name
+            </label>
+            <input
+                id="name"
+                type="text"
+                className="rounded-md my-2 p-2 bg-slate-50"
+                placeholder="Full Name"
+                value={name}
                 onChange={(e) => {
-                  handleFileChange(e);
+                setName(e.target.value);
+                setNameError(false);
                 }}
-              />
-              {parsing ? (
-                <div className="text-base text-blue-500"> Validating... </div>
-              ) : (
-                <button
-                  className="text-sm text-gray-500"
-                  onClick={(e) => {
-                    e.preventDefault();
-                  }}
-                >
-                  Browse File
-                </button>
-              )}
+                onBlur={() => {
+                if (!name) {
+                    setNameError(true);
+                }
+                }}
+            />
+            {nameError && (
+                <p className="text-red-500 text-sm">Please enter a name</p>
+            )}
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col relative mb-12">
-          <label htmlFor="content" className="text-base mb-4">
-            File Contents
-          </label>
-          <textarea
-            id="content"
-            rows={6}
-            cols={30}
-            className="overflow-auto bg-slate-100 rounded-md"
-            value={fileContent}
-            disabled
-          />
-        </div>
-        <div className="border-t-2 border-solid py-8 justify-center items-center flex">
-          <button
-            type="submit"
-            className={`bg-blue-500 rounded-full text-lg text-white font-semibold w-full p-4 ${
-              buttonDisabled ? "opacity-50" : ""
-            }`}
-            disabled={buttonDisabled}
-            onClick={(e) => {
-              e.preventDefault();
-              setIsOpen(true);
-            }}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      <Modal isOpen={isOpen} onClose={onClose} entriesCount={entriesCount} onGoToEntries={onGoToEntries} />
-    </>
-  );
+            <div className="flex flex-col relative mb-2">
+            <label htmlFor="email" className="text-base">
+                Email
+            </label>
+            <input
+                id="email"
+                type="email"
+                className="rounded-md my-2 p-2 bg-slate-50"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => handleEmailChange(e)}
+                onBlur={() => {
+                if (!isEmailValid(email)) {
+                    setEmailError(true);
+                }
+                }}
+            />
+            <BiEnvelope
+                size={28}
+                className="absolute right-4 top-10 text-sm text-slate-300"
+            />
+            {emailError && (
+                <p className="text-red-500 text-sm">Please enter a valid email</p>
+            )}
+            </div>
+            <div className="flex flex-col relative mb-2">
+            <label htmlFor="uploadFile" className="text-base mb-4">
+                Upload JSON File
+            </label>
+            <div
+                className="w-full h-32 py-8 bg-slate-100 border-2 border-dashed rounded-md"
+                onClick={(e) => {
+                fileInputRef.current.click();
+                }}
+            >
+                {parsing ? (
+                <BiLoaderCircle size={28} className="mx-auto text-blue-500" />
+                ) : (
+                <FaFileArrowUp size={28} className="mx-auto text-blue-500" />
+                )}
+                <div className="text-center">
+                <input
+                    type="file"
+                    className="hidden"
+                    accept="application/JSON"
+                    ref={fileInputRef}
+                    onChange={(e) => {
+                    handleFileChange(e);
+                    }}
+                />
+                {parsing ? (
+                    <div className="text-base text-blue-500"> Validating... </div>
+                ) : (
+                    <button
+                    className="text-sm text-gray-500"
+                    onClick={(e) => {
+                        e.preventDefault();
+                    }}
+                    >
+                    Browse File
+                    </button>
+                )}
+                </div>
+            </div>
+            </div>
+            <div className="flex flex-col relative mb-12">
+            <label htmlFor="content" className="text-base mb-4">
+                File Contents
+            </label>
+            <textarea
+                id="content"
+                rows={6}
+                cols={30}
+                className="overflow-auto bg-slate-100 rounded-md"
+                value={fileContent}
+                disabled
+            />
+            </div>
+            <div className="border-t-2 border-solid py-8 justify-center items-center flex">
+            <button
+                type="submit"
+                className={`bg-blue-500 rounded-full text-lg text-white font-semibold w-full p-4 ${
+                buttonDisabled ? "opacity-50" : ""
+                }`}
+                disabled={buttonDisabled}
+                onClick={(e) => {
+                e.preventDefault();
+                setIsOpen(true);
+                }}
+            >
+                Submit
+            </button>
+            </div>
+        </form>
+        <Modal isOpen={isOpen} onClose={onClose} entriesCount={entriesCount} onGoToEntries={onGoToEntries} />
+        </>
+    );
 };
--- components/Modal.js
+++ components/Modal.js
@@ -1,38 +1,38 @@
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
 const Modal = ({ isOpen, onClose, entriesCount, onGoToEntries }) => {
-  const modalClass = isOpen
-    ? "fixed inset-0 flex items-center justify-center z-50"
-    : "hidden";
+    const modalClass = isOpen
+        ? "fixed inset-0 flex items-center justify-center z-50"
+        : "hidden";
 
-  return (
-    <div className={modalClass}>
-      <div className="modal-overlay absolute inset-0 bg-black opacity-50"></div>
-      <div className="modal-container bg-white w-11/12 max-w-sm mx-auto rounded-3xl shadow-lg z-50 overflow-y-auto">
-        <div className="modal-content py-4 px-6 flex flex-col justify-center items-center">
-          <BsFillCheckCircleFill size={70} className="text-blue-500 mt-4" />
-          <h2 className="text-xl text-blue-500 font-bold text-center py-2">
-            Success!
-          </h2>
-          <div className="text-base text-black text-center font-semibold py-2 mb-4">
-            {entriesCount} entries successfully uploaded
-          </div>
-          <button
-            className="w-full bg-blue-500 p-4 rounded-full text-base font-semibold text-white mb-4"
-            onClick={onGoToEntries}
-          >
-            Go To My Entries
-          </button>
-          <button
-            className="w-full bg-blue-200 p-4 rounded-full text-base font-semibold  text-blue-500 mb-4"
-            onClick={onClose}
-          >
-            Cancel
-          </button>
+    return (
+        <div className={modalClass}>
+        <div className="modal-overlay absolute inset-0 bg-black opacity-50"></div>
+        <div className="modal-container bg-white w-11/12 max-w-sm mx-auto rounded-3xl shadow-lg z-50 overflow-y-auto">
+            <div className="modal-content py-4 px-6 flex flex-col justify-center items-center">
+            <BsFillCheckCircleFill size={70} className="text-blue-500 mt-4" />
+            <h2 className="text-xl text-blue-500 font-bold text-center py-2">
+                Success!
+            </h2>
+            <div className="text-base text-black text-center font-semibold py-2 mb-4">
+                {entriesCount} entries successfully uploaded
+            </div>
+            <button
+                className="w-full bg-blue-500 p-4 rounded-full text-base font-semibold text-white mb-4"
+                onClick={onGoToEntries}
+            >
+                Go To My Entries
+            </button>
+            <button
+                className="w-full bg-blue-200 p-4 rounded-full text-base font-semibold  text-blue-500 mb-4"
+                onClick={onClose}
+            >
+                Cancel
+            </button>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Modal;
